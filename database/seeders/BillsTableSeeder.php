7772690f3772ef146566bd9be360b41a384a89f1
VanDuyqwe1 <voduy12345duyv@gmail.com>
2023-05-07 22:49:55 +0700

    Update best sale homepage version-1

---
+++ database/seeders/BillsTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class BillsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table('bills')->insert(
+            [
+                ['id_user'=>'1','id_status'=>'6','id_voucher'=>null,'address'=>'Linh đông',],
+                ['id_user'=>'2','id_status'=>'4','id_voucher'=>null,'address'=>'Linh đông',],
+                ['id_user'=>'3','id_status'=>'7','id_voucher'=>null,'address'=>'Linh đông',],
+                ['id_user'=>'4','id_status'=>'3','id_voucher'=>null,'address'=>'Linh đông',],
+                ['id_user'=>'5','id_status'=>'6','id_voucher'=>null,'address'=>'Linh đông',],
+                ['id_user'=>'6','id_status'=>'1','id_voucher'=>null,'address'=>'Linh đông',],
+                ['id_user'=>'7','id_status'=>'2','id_voucher'=>null,'address'=>'Linh đông',],
+                ['id_user'=>'8','id_status'=>'7','id_voucher'=>null,'address'=>'Linh đông',],
+                ['id_user'=>'9','id_status'=>'1','id_voucher'=>null,'address'=>'Linh đông',],
+
+                ['id_user'=>'10','id_status'=>'1','id_voucher'=>2001,'address'=>'Linh đông',],
+                ['id_user'=>'11','id_status'=>'2','id_voucher'=>2002,'address'=>'Linh đông',],
+                ['id_user'=>'12','id_status'=>'3','id_voucher'=>2003,'address'=>'Linh đông',],
+                ['id_user'=>'13','id_status'=>'4','id_voucher'=>2004,'address'=>'Linh đông',],
+                ['id_user'=>'14','id_status'=>'5','id_voucher'=>2005,'address'=>'Linh đông',],
+                ['id_user'=>'15','id_status'=>'6','id_voucher'=>2006,'address'=>'Linh đông',],
+                
+            ]
+        );
+    }
+}
---
+++ public/blocks/1611/1611-content.php
@@ -0,0 +1,107 @@
+
+<?php
+$url_path = "http://fit.tdc.edu.vn/courses/cdw2/tai/code_module_1600_v2/blocks/1611";
+?>
+
+<div class="type-1611">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="collection">
+                        <ul>
+                            <li>
+                               <div class="image-intagram">
+                                   <a href="#">
+                                       <i class="fa fa-instagram"></i>
+                                       <p>Công ty 1</p>
+                                   </a>
+
+                                   <img src="<?php echo $url_path ?>/images/01_Home1_in_120x120.jpg" alt="itn1">
+                               </div>
+                            </li>
+                            <li>
+                                <div class="image-intagram">
+                                    <a href="#">
+                                        <i class="fa fa-instagram"></i>
+                                        <p>Công ty 2</p>
+                                    </a>
+
+                                    <img src="<?php echo $url_path ?>/images/02_Home1_in_120x120.jpg" alt="itn2">
+                                </div>
+                            </li>
+                            <li>
+                                <div class="image-intagram">
+                                    <a href="#">
+                                        <i class="fa fa-instagram"></i>
+                                        <p>Công ty 2</p>
+                                    </a>
+
+                                    <img src="<?php echo $url_path ?>/images/03_Home1_in_120x120.jpg" alt="itn3">
+                                </div>
+                            </li>
+                            <li>
+                                <div class="image-intagram">
+                                    <a href="#">
+                                        <i class="fa fa-instagram"></i>
+                                        <p>Công ty 2</p>
+                                    </a>
+                                    <img src="<?php echo $url_path ?>/images/04_Home1_in_120x120.jpg" alt="itn4">
+                                </div>
+                            </li>
+                            <li>
+                                <div class="image-intagram">
+                                    <a href="#">
+                                        <i class="fa fa-instagram"></i>
+                                        <p>Công ty 2</p>
+                                    </a>
+                                    <img src="<?php echo $url_path ?>/images/05_Home_in_120x120.jpg" alt="itn5">
+                                </div>
+                            </li>
+                            <li>
+                                <div class="image-intagram">
+                                    <a href="#">
+                                        <i class="fa fa-instagram"></i>
+                                        <p>Công ty 2</p>
+                                    </a>
+                                    <img src="<?php echo $url_path ?>/images/06_Home1_in_120x120.jpg" alt="itn6">
+                                </div>
+                            </li>
+                            <li>
+                                <div class="image-intagram">
+                                    <a href="#">
+                                        <i class="fa fa-instagram"></i>
+                                        <p>Công ty 2</p>
+                                    </a>
+                                    <img src="<?php echo $url_path ?>/images/07_Home1_in_120x120.jpg" alt="itn7">
+                                </div>
+                            </li>
+                            <li>
+                                <div class="image-intagram">
+                                    <a href="#">
+                                        <i class="fa fa-instagram"></i>
+                                        <p>Công ty 2</p>
+                                    </a>
+                                    <img src="<?php echo $url_path ?>/images/08_Home1_in_120x120.jpg" alt="itn8">
+                                </div>
+                            </li>
+                            <li>
+                                <div class="image-intagram">
+                                    <a href="#">
+                                        <i class="fa fa-instagram"></i>
+                                        <p>Công ty 2</p>
+                                    </a>
+                                    <img src="<?php echo $url_path ?>/images/09_Home1_in_120x120.jpg" alt="itn9">
+                                </div>
+                            </li>
+
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+
+</div>
