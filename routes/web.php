fbec277faadc15dad899ff8dc815da58e0241a15
VanDuyqwe1 <voduy12345duyv@gmail.com>
2023-05-14 14:41:24 +0700

    update menu

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
-
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CustomAuthController;
-
-
-use App\Http\Controllers\VoucherController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BillController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -21,37 +13,7 @@ use App\Http\Controllers\BillController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-
-Route::get('/', [ProductController::class, 'index']);
-=======
-Route::get('dashboard', [CustomAuthController::class, 'dashboard']); 
-Route::get('/', [CustomAuthController::class, 'dashboard']); 
-
-Route::get('login', [CustomAuthController::class, 'index'])->name('login');
-Route::get('voucher', [CustomAuthController::class, 'voucher'])->name('voucher');
-Route::get('promotion', [CustomAuthController::class, 'promotion'])->name('promotion');
-Route::get('menu', [CustomAuthController::class, 'menu'])->name('menu');
-Route::get('orderTracking', [CustomAuthController::class, 'orderTracking'])->name('orderTracking');
-Route::get('storeListing', [CustomAuthController::class, 'storeListing'])->name('storeListing');
-Route::get('blog', [CustomAuthController::class, 'blog'])->name('blog');
-Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom'); 
-Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
-Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom'); 
-Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
-
-//  Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/tracking', [BillController::class, 'index']);
-Route::get('/tracking/{slug}/{id_status}', [BillController::class, 'show'])->name('tracking_custom');
-
+Route::get('thucdon', [ProductController::class, 'showCategoryRoot']);
+Route::get('thucdon/{id_category}', [ProductController::class, 'showCategoryRoot']);
+Route::get('thucdon/{id_category}', [ProductController::class, 'showProductById'])->name('category-products-custom');
 
