652cdd0d248a6dcc5d6502f9ea6884f334193e9a
nguyenquoctien87 <21211tt3839@mail.tdc.edu.vn>
2023-05-14 10:57:36 +0700

    admin_count

--- database/migrations/2023_05_07_140810_create_admins_table.php
+++ database/migrations/2023_05_07_140810_create_admins_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name',100);
+            $table->string('email',100)->unique();
             $table->string('password');
             $table->timestamps();
         });
